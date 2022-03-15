32918365069ce10e6db6ec7f73ab718edb4ed3f8
Caillloux Alexandre <alexandre.cailloux@etu.univ-orleans.fr>
2022-03-15 17:44:46 +0100

    Generation d un fichier binaire

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include "recupParam.hpp"
 #include "circuit.hpp"
+#include <fstream>
+#include <stdlib.h>
 
 using namespace std;
+
+void CheminALaMain(){
+    std::ofstream myFile ("chemin.bin", std::ios::out | std::ios::binary);
+    int vitesse = 1;
+    int valeurJ = 0;
+    int baseJ = 0;
+    int x = 2;
+    int y = 2;
+    for(int i = 0; i<49; i++){
+
+        myFile.write (reinterpret_cast<const char *>(&x), sizeof(x));
+        myFile.write (reinterpret_cast<const char *>(&y), sizeof(y));
+        std::cout << "(" << x << "," << y << ")" <<'\n';
+
+    }
+    x = 1;
+    y = 1;
+    myFile.write (reinterpret_cast<const char *>(&x), sizeof(x));
+    myFile.write (reinterpret_cast<const char *>(&y), sizeof(y));
+    std::cout << "(" << x << "," << y << ")" <<'\n';
+    myFile.close();
+}
 
 int main() {
 
@@ -28,6 +52,14 @@ int main() {
   std::map<std::string, int> depart = (*recupParam).getCoordonneesDepart();
 
   std::cout << std::endl;
+
+  CheminALaMain();
+
+  int lecture;
+
+  std::ifstream myFileToRead ("chemin.bin", std::ios::out | std::ios::binary);
+  myFileToRead.read (reinterpret_cast<char *>(&lecture), sizeof(lecture));
+  std::cout << (int)lecture << std::endl;
 
 
   delete recupParam;
